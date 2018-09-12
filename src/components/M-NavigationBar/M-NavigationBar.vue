e53832128e1ce624bbd70e0a018c49cbd03a1aac
Lucas <lucasdanielotero1992@gmail.com>
2018-09-12 02:34:17 -0300

    first commit

---
+++ src/components/M-NavigationBar/M-NavigationBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="navigation-bar">
+
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    links: {
+      type: Array,
+      required: false
+    }
+  }
+};
+</script>
+
+<style scoped lang="sass">
+$primary-background: #3eaf7e
+$primary-background__dark: #27664b
+
+#navigation-bar
+  width: 100%
+  height: 80px
+  background-color: $primary-background
+  border-bottom: $primary-background__dark 2px solid
+</style>
